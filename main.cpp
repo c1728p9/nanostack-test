25fb5ea124a920e177d01a9a7f1b68724f394afa
Russ Butler <russ.butler@arm.com>
2016-06-12 19:44:27 +0100

    Update to support TCP
    Update code to the latest mbed-os and add support for TCP.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 
 #include "net_interface.h"
 #include "socket_api.h"
+#include "UDPSocket.h"
+#include "TCPSocket.h"
+#include "TCPServer.h"
 
 #undef ETHERNET
 #undef WIFI
@@ -29,67 +32,108 @@ ThreadInterface mesh;
 
 Serial output(USBTX, USBRX);
 
-osThreadId mainThread;
-
-int8_t socket = 0;
-
 // Status indication
 Ticker status_ticker;
 DigitalOut status_led(LED1);
 DigitalOut command_led(LED2);
 void blinky() { status_led = !status_led; }
 
-void socket_data(void * cb)
+static Queue<TCPSocket, 16> tcp_main_queue;
+
+void udp_main(const void *net)
 {
-    ns_address_t source_addr;
+    NetworkStack *network = (NetworkStack*)net;
+
+    UDPSocket sock(network);
+    SocketAddress source_addr;
     uint8_t buffer[32];
-	output.printf("Packet!\r\n");
-    socket_callback_t *cb_res = (socket_callback_t *)cb;
-    int16_t length;
+    uint8_t * data = NULL;
+    int length = 0;
 
-    if( cb_res->socket_id != socket ) {
-        // This isn't our socket
-        output.printf("Message for wrong socket: %i\r\n", cb_res->socket_id);
-        return;
-    }
-
-    if( cb_res->event_type == SOCKET_DATA ) {
-        //Read data from the RX buffer
+    //sock.attach(fp);
+    sock.set_blocking(true);
+    sock.bind(1234);
+    TCPSocket *local_ptr = NULL;
+    while (true) {
         memset(buffer, 0, sizeof(buffer));
-        length = socket_read( cb_res->socket_id,
-                &source_addr,
-                buffer,
-                sizeof(buffer) - 1);
-
-        if (length) {
-            output.printf("Received message \"%s\"\r\n", buffer);
-            uint8_t * data = (uint8_t *)"Packet recieved\r\n";
-            socket_sendto(cb_res->socket_id, &source_addr, data, strlen((char*)data));
+        length = sock.recvfrom(&source_addr, buffer, sizeof(buffer) - 1);
+        if (length > 0) {
+            //output.printf("Socket Addr: %s\r\n", source_addr.get_ip_address());
+            data = (uint8_t *)"Packet recieved\r\n";
+            int ret = sock.sendto(source_addr, data, strlen((char*)data));
+            //output.printf("Send returned %i\r\n", ret);
             if (strcmp((char*)buffer, "on") == 0) {
                 command_led = 0;
             }
             if (strcmp((char*)buffer, "off") == 0) {
                 command_led = 1;
             }
-
+            if (strcmp((char*)buffer, "close") == 0) {
+                if (local_ptr != NULL) {
+                    local_ptr->close();
+                    local_ptr = NULL;
+                }
+            }
+            uint16_t port = 0;
+            if (1 == sscanf((char*)buffer, "connect %hu", &port)) {
+                if (local_ptr != NULL) {
+                    local_ptr->close();
+                }
+                SocketAddress addr(source_addr);
+                addr.set_port(port);
+                local_ptr = new TCPSocket(network);
+                ret = local_ptr->connect(addr);
+                //output.printf("Connect returned %i\r\n", ret);
+                if (0 == ret) {
+                    tcp_main_queue.put(local_ptr);
+                } else {
+                    delete local_ptr;
+                    local_ptr = NULL;
+                }
+            }
         }
     }
+}
 
+void tcp_main(const void *net)
+{
+    while (true) {
+        osEvent event = tcp_main_queue.get();
+        if (event.status != osEventMessage) {
+            //output.printf("Invalid return %i\r\n", event.status);
+            continue;
+        }
+        //output.printf("Socket recieved\r\n");
+
+        TCPSocket *socket = (TCPSocket *)event.value.p;
+        while (true) {
+            uint8_t data[64];
+            memset(data, 0, sizeof(data));
+            int ret = socket->recv(data, sizeof(data));
+            if (ret > 0) {
+                //output.printf("Read data: %s\r\n", data);
+            } else {
+                //output.printf("Ret: %i\r\n", ret);
+                break;
+            }
+            char resp[] = "Got message 2";
+            ret = socket->send(resp, sizeof(resp));
+            //output.printf("Send ret: %i\r\n", ret);
+        }
+        delete socket;
+    }
 }
 
 // Entry point to the program
 int main() {
     status_ticker.attach_us(blinky, 250000);
 
-    // Keep track of the main thread
-    mainThread = osThreadGetId();
-
     // Sets the console baud-rate
     output.baud(115200);
 
     output.printf("Starting Nanostack example\r\n");
 
-    NetworkInterface *network_interface = 0;
+    NetworkStack *network_interface = 0;
 #if defined WIFI
     output.printf("\n\rUsing WiFi \r\n");
     output.printf("\n\rConnecting to WiFi..\r\n");
@@ -109,19 +153,15 @@ int main() {
     if (ip_addr) {
         output.printf("IP address %s\r\n", ip_addr);
     } else {
-        output.printf("No IP address %s\r\n");
+        output.printf("No IP address\r\n");
     }
 
-    socket = socket_open(SOCKET_UDP, 1234, socket_data);
-    if (socket < 0) {
-    	output.printf("Error opening socket: %i", socket);
-    }
+    Thread udp_thread(udp_main, (void*)network_interface);
+    Thread tcp_thread(tcp_main, (void*)network_interface);
+
+    status_ticker.detach();
 
     while (true) {
         rtos::Thread::wait(1000);
     }
-
-    status_ticker.detach();
 }
-
-
--- send_over_tcp.py
+++ send_over_tcp.py
@@ -1,5 +1,6 @@
 import socket
 import argparse
+import threading
 
 DEFAULT_PORT = 1234
 #https://docs.python.org/2/howto/sockets.html
@@ -10,19 +11,39 @@ parser.add_argument('--port', type=int, default=DEFAULT_PORT,
 parser.add_argument('--addr', type=str, required=True,
                     help="Addr to send to E.X. "
                     "'fd00:ff1:ce0b:a5e0:fec2:3d00:4:ea8c'")
-parser.add_argument('--data', type=str, required=True,
-                    help="Message to send")
 args = parser.parse_args()
 
 addr = args.addr
 port = args.port
-data = args.data
+
+done = False
+
+def read_socket(socket):
+    while (True):
+        data = socket.recv(4096)
+        if len(data) > 0:
+            print("Data recieved: %s" % data)
+        if done:
+            print("Read thread exiting")
+            break
+        
+
 
 #HOST = socket.gethostname()
 #info = socket.getaddrinfo(HOST, 1234)
 #print("Info: %s" % info)
 sock = socket.socket(socket.AF_INET6, # Internet
-                     socket.SOCK_DGRAM) # UDP
+                     socket.SOCK_STREAM)
 #sock.bind(('', 1234))
-sock.sendto(data, (addr, port))
-print("Message sent")
+sock.connect((addr, port))
+read_thread = threading.Thread(target=read_socket, args=(sock,))
+read_thread.daemon = True
+read_thread.start()
+try:
+    while (True):
+        data = raw_input("Enter command:")
+        sock.send(data)
+        print("Message sent: %s" % data)
+finally:
+    sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
